796244847ca8be356de4458ae265335098f325d8
le0nidas0 <leonidasmoreira098@gmail.com>
2024-04-25 19:09:37 -0300

    Crud_Curso

--- demo/src/main/java/com/squad_17_api/demo/Curso.java
+++ demo/src/main/java/com/squad_17_api/demo/Curso.java
@@ -1,2 +1,25 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cursos")
 public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCurso;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+
+    @Column(name = "descricao", length = 500)
+    private String descricao;
+
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+
 }
