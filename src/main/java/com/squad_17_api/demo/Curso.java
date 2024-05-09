d5098e8dc552a2ee04ccf809e0971d3f1682e088
le0nidas0 <leonidasmoreira098@gmail.com>
2024-05-09 18:04:06 -0300

    Update Curso.java

--- src/main/java/com/squad_17_api/demo/Curso.java
+++ src/main/java/com/squad_17_api/demo/Curso.java
@@ -23,8 +23,32 @@ public class Curso {
     @Column(name = "ativo", nullable = false)
     private boolean ativo;
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
 }
