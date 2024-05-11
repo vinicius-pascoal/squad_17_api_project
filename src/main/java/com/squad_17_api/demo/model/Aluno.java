964693fdff21712db29df884e185cc931f776572
guilobodev <guilhermelobobrayner@gmail.com>
2024-05-11 15:03:36 -0300

    atualizando crud alunos

--- src/main/java/com/squad_17_api/demo/model/Aluno.java
+++ src/main/java/com/squad_17_api/demo/model/Aluno.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+//model
 @Entity
 public class Aluno {
     @Id
